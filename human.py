c2aecb4dc5dd9966447444862ac8202dc589569d
StephanieCobble <cobble.stephanie@gmail.com>
2022-03-07 14:17:00 -0800

    Updated human class to allow for chosen gestures and print appropriate results

--- human.py
+++ human.py
@@ -3,3 +3,17 @@ from player import Player
 class Human(Player):
     def __init__(self):
         super().__init__()
+        self.score = 0
+
+    def choose_gesture(self):
+        gesture_index = 0
+        for gesture in self.gestures:
+            print(f"Choose {gesture_index} for {gesture}: ")
+            gesture_index += 1
+        human_input = int(input("Choose your gesture! "))
+        if human_input in ("0", "1", "2", "3", "4"):
+            self.chosen_gesture = human_input
+            print(f"Player one chose {self.gestures[int(self.chosen_gesture)]}")
+        else:
+            print("Please enter number value 0-4 to choose gesture")
+            self.choose_gesture()
