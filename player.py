bec1c2e47d1a4ef125eccc242dbe118d95369788
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-03-08 11:04:27 -0500

    Added a score variable to the Player class and a corresponding method that increments that score if called.

--- player.py
+++ player.py
@@ -1,5 +1,9 @@
 class Player ():
     def __init__(self):
         self.gestures = ["rock","paper","scissors","spock","lizard"]
+        self.score = 0
+
+    def increment_score(self):
+        self.score += 1
     
     
